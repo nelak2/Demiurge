5405a2e1edf0693ae7c4011043036ec4641ee255
NelaK <hello468@hotmail.com>
2012-04-26 14:38:22 -0600

    Corrected dependencies

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SharpDX.Windows;
 
 namespace Client
 {
@@ -9,6 +10,9 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            var form = new RenderForm("Test");
+
+            
         }
     }
 }
